15a1d9b8e035d0c53f3f74be6ec50ea524e2458a
Macwan Nevil <macnev2013@gmail.com>
2021-07-20 12:59:11 +0530

    added ui improvements
    * added ui improvements
    
    * minor fix
    
    * removed unused imports

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
+#!/bin/bash
 VENV_DIR ?= .venv
-PIP_CMD ?= pip
+PIP_CMD ?= pip3
+PYTHON_VERSION ?= python3
 
 ifeq ($(OS), Windows_NT)
 	VENV_RUN = . $(VENV_DIR)/Scripts/activate
@@ -9,18 +11,20 @@ endif
 
 setup-venv:
 	(test `which virtualenv` || $(PIP_CMD) install --user virtualenv) && \
-		(test -e $(VENV_DIR) || virtualenv $(VENV_OPTS) $(VENV_DIR))
+		(test -e $(VENV_DIR) || ${PYTHON_VERSION} -m virtualenv $(VENV_OPTS) $(VENV_DIR))
 
 install-venv:
 	make setup-venv && \
 		test ! -e requirements.txt || ($(VENV_RUN); $(PIP_CMD) -q install -r requirements.txt)
 
-
 install:
 	(make install-venv) || exit 1
 
 run:             ## Manually start the local infrastructure for testing
-	($(VENV_RUN); python manage.py migrate; python manage.py runserver)
+	(mkdir workspace || echo "Workspace already exsists"; $(VENV_RUN); python manage.py migrate; python manage.py runserver)
+
+create-super-user:
+	($(VENV_RUN); python manage.py createsuperuser)
 
 clean:             ## Clean up (npm dependencies, downloaded infrastructure code, compiled Java classes)
 	rm -rf $(VENV_DIR)
--- automation/forms.py
+++ automation/forms.py
@@ -1,13 +1,13 @@
 from django import forms
 
 class AutomationForm(forms.Form):
-    project_name = forms.CharField(label='Project Name', max_length=100)
-    repo_url = forms.CharField(label='Repo Git URL', max_length=2083)
+    project_name = forms.CharField(label='', max_length=100, widget=forms.TextInput(attrs={'class': 'custom-input', 'placeholder': 'Project Name'}))
+    repo_url = forms.CharField(label='', max_length=2083, widget=forms.TextInput(attrs={'class': 'custom-input', 'placeholder': 'Repo Git URL'}))
 
 class PatchJenkinsfile(forms.Form):
-    jenkinsfile = forms.CharField(label='Jenkinsfile', widget=forms.Textarea(attrs={'cols': 100, 'rows': 20}), required=False)
-    branches = forms.ChoiceField(widget=forms.Select())
-    commit_message = forms.CharField(label='Commit Message', max_length=100, required=False)
+    branches = forms.ChoiceField(label='Branch', widget=forms.Select(attrs={'class': 'custom-input mb-3'}))
+    jenkinsfile = forms.CharField(label='Jenkinsfile', widget=forms.Textarea(attrs={'class': 'custom-input', 'cols': 100, 'rows': 20, 'id': 'jenkinsfileTextArea',}), required=False)
+    commit_message = forms.CharField(label='', max_length=100, required=False, widget=forms.TextInput(attrs={'class': 'custom-input mt-3', 'placeholder': 'Commit Message'}))
 
     def set_branches(self, branches=None):
         self.fields['branches'].choices = [(branch, branch) for branch in branches]
